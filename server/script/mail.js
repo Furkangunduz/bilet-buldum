197af5ca0fb708f2a3d4ae5a39ce7cbcd233e27e
Furkan <Furkan.gundz00@gmail.com>
2022-06-07 23:02:13 +0300

    Created mail service.

---
+++ server/script/mail.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.APP_PASSWORD,
+    }
+});
+const sendMail = (to, { subject, text }) => {
+    var mailOptions = {
+        from: process.env.EMAIL,
+        to,
+        subject,
+        text,
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) console.log("Mail Gönderirken bir sorunla karşılaşıldı");
+        else console.log('Mail başarıyla gönderildi');
+    });
+}
+
+module.exports = { sendMail }
