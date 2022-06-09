f36c5e04c2f5e45e372f305dc5af4ab8947007c6
Furkan <Furkan.gundz00@gmail.com>
2022-06-09 15:28:44 +0300

    created react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import Search from "./pages/Search";
+import Home from "./pages/Home";
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/search" element={<Search />} />
+  </Routes>
 }
 
 export default App;
