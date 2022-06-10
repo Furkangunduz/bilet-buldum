99c4e8c99ddc66c48e5e988e2b0ec58769cdd5e5
Furkan <Furkan.gundz00@gmail.com>
2022-06-10 22:36:24 +0300

    New Homepage.

--- client/src/components/Select.jsx
+++ client/src/components/Select.jsx
@@ -1,8 +1,14 @@
-const Select = ({ direction, setSearchData }) => {
+const Select = ({ direction, passData }) => {
 	return (
 		<div>
-			<label htmlFor={direction}>{direction}</label>
-			<select id={direction} required>
+			<label htmlFor={direction}>{direction}:</label>
+			<select
+				onChange={(e) => {
+					passData(e.target.value);
+				}}
+				id={direction}
+				required>
+				<option value='' defaultChecked></option>
 				<option value='İzmit'>İzmit</option>
 				<option value='Ankara'>Ankara</option>
 			</select>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,31 @@
+import { Link } from 'react-router-dom';
+
 const Home = () => {
-	return <div>Henüz Hazır Değil</div>;
+	return (
+		<div
+			style={{
+				display: 'flex',
+				flexDirection: 'column',
+				justifyContent: 'center',
+				alignItems: 'center',
+				gap: '20px',
+			}}>
+			<p>Henüz tamamlanmadı . . .</p>
+			<p>Arama yapmak için :</p>
+			<Link to='/search'>
+				<button
+					style={{
+						padding: '20px 30px',
+						backgroundColor: 'purple',
+						color: 'white',
+						borderRadius: '10px',
+						cursor: 'pointer',
+					}}>
+					Arama yap
+				</button>
+			</Link>
+		</div>
+	);
 };
 
 export default Home;
