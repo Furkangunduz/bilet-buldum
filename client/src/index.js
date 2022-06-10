3f22d0f89ebe178e708dc03d2faf472d9785fb74
Furkan <Furkan.gundz00@gmail.com>
2022-06-10 22:35:05 +0300

    update

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './style.css';
+import './style/style.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 
