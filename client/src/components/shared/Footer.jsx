20e7b6f452960ecb6bfdcffefe2d05cf479a9d82
Furkan <Furkan.gundz00@gmail.com>
2022-06-13 01:02:33 +0300

    .

--- client/src/components/shared/Footer.jsx
+++ client/src/components/shared/Footer.jsx
@@ -5,7 +5,8 @@ function footer() {
 				<div>Developed by</div>
 				<div>
 					<a href='https://www.linkedin.com/in/hakankasap1/'> Hakan Kasap </a> &{' '}
-					<a href='https://www.linkedin.com/in/furkangunduz00/'> Furkan Gündüz </a>
+					<a href='https://www.linkedin.com/in/furkangunduz00/'> Furkan Gündüz </a>&
+					<a href='https://www.linkedin.com/in/furkan-kasap/'> Furkan Kasap </a>
 				</div>
 			</div>
 		</footer>
