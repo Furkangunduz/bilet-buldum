a8b95f5c3d0bef4b39c1c0914ff7a3c8b5b57b02
Furkan <furkan.gundz00@gmail.com>
2022-08-21 17:47:46 +0300

    GetData from json

--- client/src/components/Home/CardItem.jsx
+++ client/src/components/Home/CardItem.jsx
@@ -1,6 +1,6 @@
 import user from '../../img/user.svg';
 
-function CardItem() {
+function CardItem({ comment }) {
 	return (
 		<div className='flex overflow-y-hidden flex-col flex-shrink-0 text-black  md:w-[300px] md:h-[250px] xs:w-[250px] xs:h-[220px]  rounded-md bg-white box shadow-2xl'>
 			<div className='flex'>
@@ -11,15 +11,12 @@ function CardItem() {
 						alt='profile-pic'
 					/>
 				</div>
-				<div className='ml-10 mt-6 font-bold text-md text-black/90 tracking-wider'>
-					<h3>AD SOYAD</h3>
+				<div className='ml-6 mt-6 font-bold text-md text-black/90 tracking-wider'>
+					<h3>{comment.name}</h3>
 				</div>
 			</div>
-			<div className='mt-6 mx-4 text-sm text-left'>
-				<p>
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi debitis sunt
-					eos illo hic voluptas cupiditate in dicta dolores dignissimos alias
-				</p>
+			<div className='mt-8 mx-4 text-xs text-left'>
+				<p>{comment.text}</p>
 			</div>
 		</div>
 	);
---
+++ client/src/data/comments.js
@@ -0,0 +1,9 @@
+const comments = [
+    { name: "Mücahit Kocaman", text: "Bilet buldum olmasa haftalar sonrasına bilet almak zorunda kalacaktım. Süper bir uygulama" },
+    { name: "Nuriye Nabi", text: "Bilet buldum sayesinde bilet bulmak için uğraşmama gerek kalmadı. Tek tıkla her şeyi hallettim" },
+    { name: "Sevinç Akbay", text: "Çoğu zaman ertesi gün için bütün biletler alınmış oluyordu o yüzden çok önceden plan yapmam gerekiyordu. Artık aynı gün içinde bile tren bileti bulabiliyorum." },
+    { name: "Miray Samdereli", text: "Tek kelimeyle harika." },
+    { name: "Serhat Deliktas", text: "Bilet buldum olmasa haftalar sonrasına bilet almak zorunda kalacaktım. Süper bir uygulama" },
+]
+
+export default comments
